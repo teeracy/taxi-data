b83c6a7641bc643f89aee44897bbd1c756487134
Tracy Huynh <tracythuynh@gmail.com>
2014-08-20 13:46:06 -0400

    render map

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+var map;
+
+function initialize() {
+  var mapOptions = {
+    zoom: 14,
+    center: new google.maps.LatLng(40.732751, -73.922195)
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
 $(function() {
 	$.getJSON("intersections-taxi-data.json", function(json) {
-	    console.log(json); // this will show the info it in firebug console
+	    console.log(json); 
+	    for (i = 0; i < (json.length/1000); i++) {
+	    	var chances = (json[i].passEmpties) / (json[i].passEmpties + json[i].passFulls)
+	    	var circleOptions = {
+	    		strokeColor: '#FF0000',
+		      	strokeOpacity: 0.8,
+		      	strokeWeight: 2,
+		      	fillColor: '#FF0000',
+		      	fillOpacity: 0.35,
+		      	map: map,
+		      	center: new google.maps.LatLng(json[i].geo.coordinates[1], json[i].geo.coordinates[0]),
+		      	radius: chances * 100
+	    	}
+	    intersectionCircle = new google.maps.Circle(circleOptions);
+	    }
 	});
 });
-console.log("Hi")
+
+
